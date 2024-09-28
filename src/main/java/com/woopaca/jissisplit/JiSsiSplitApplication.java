13ca5eed794fbb8bc0754dea2a5e69908b9bcdc5
jcw1031 <jcw001031@gmail.com>
2024-09-28 12:43:21 +0900

    initial commit

---
+++ src/main/java/com/woopaca/jissisplit/JiSsiSplitApplication.java
@@ -0,0 +1,13 @@
+package com.woopaca.jissisplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiSsiSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiSsiSplitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woopaca/jissisplit/JiSsiSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woopaca.jissisplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiSsiSplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
