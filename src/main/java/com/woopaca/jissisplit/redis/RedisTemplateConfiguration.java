c9ba98c9437e1554bf5fe12f4164ca0bb110b7e4
jcw1031 <jcw001031@gmail.com>
2024-10-25 19:12:26 +0900

    refactor: RedisTemplate Serializer 설정 및 빈 등록
    * key는 문자열 직렬화, value는 객체 JSON 직렬화 설정
    * valueSerializer로 Jackson2JsonRedisSerializer를 등록해 객체가 JSON 형태로 변환되어 저장되도록 설정
      - Spring에서 기본으로 등록한 ObjectMapper를 사용하기 위해 의존성 주입

---
+++ src/main/java/com/woopaca/jissisplit/redis/RedisTemplateConfiguration.java
@@ -0,0 +1,23 @@
+package com.woopaca.jissisplit.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisTemplateConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        Jackson2JsonRedisSerializer<Object> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+        redisTemplate.setValueSerializer(valueSerializer);
+        return redisTemplate;
+    }
+}
