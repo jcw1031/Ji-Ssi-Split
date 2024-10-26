03d2284966e1b4322dd0d4cc9ab77a72cdef3c64
jcw1031 <jcw001031@gmail.com>
2024-10-26 11:19:25 +0900

    feature: 매도 및 매수 조건 확인 후 처리 로직 틀 작성
    * 최고가 갱신
    * 만약 최고가가 트레일링 스탑 활성화 변동률을 만족하면 트레일링 스탑 활성화
    * 최고점에서 설정된 비율 이하로 하락하면 매도
    * 구매 가격보다 주식 가격이 설정된 비율 이하로 하락하면 매수

---
+++ src/main/java/com/woopaca/jissisplit/processor/StockPriceMessageListener.java
@@ -0,0 +1,104 @@
+package com.woopaca.jissisplit.processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woopaca.jissisplit.collector.websocket.dto.StockPriceResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Slf4j
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class StockPriceMessageListener implements MessageListener {
+
+    private final ObjectMapper objectMapper;
+
+    private BigDecimal maxPrice;
+    private BigDecimal purchasePrice;
+    private BigDecimal activationThreshold;
+    private BigDecimal trailingStopRate;
+    private BigDecimal dipRate;
+    private BigDecimal purchaseAmount;
+
+    public StockPriceMessageListener(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.maxPrice = BigDecimal.ZERO;
+        this.purchasePrice = BigDecimal.ZERO;
+        this.activationThreshold = BigDecimal.valueOf(1.0);
+        this.trailingStopRate = BigDecimal.valueOf(0.3);
+        this.dipRate = BigDecimal.valueOf(1.0);
+        this.purchaseAmount = BigDecimal.valueOf(1_000.00);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            StockPriceResponse body = objectMapper.readValue(message.getBody(), StockPriceResponse.class);
+            BigDecimal stockPrice = body.stockPrice();
+            if (purchasePrice.equals(BigDecimal.ZERO)) {
+                purchasePrice = stockPrice;
+            }
+
+            if (isLessThan(purchasePrice, stockPrice)) { // maxPrice 비교 및 갱신 및 트레일링 스탑 활성 여부 확인
+                if (isLessThan(maxPrice, stockPrice)) { // maxPrice 비교 및 갱신
+                    maxPrice = stockPrice;
+                    return;
+                }
+
+                BigDecimal increaseRate = calculateIncreaseRate(purchasePrice, maxPrice);
+                if (isLessThan(increaseRate, activationThreshold)) { // 트레일링 스탑 활성화 여부 확인
+                    return;
+                }
+
+                BigDecimal dropRate = calculateDropRate(maxPrice, stockPrice);
+                if (isLessThan(trailingStopRate, dropRate)) { // 트레일링 스탑 조건 만족 확인
+                    // TODO 매도
+                }
+                return;
+            }
+
+            BigDecimal dropRate = calculateDropRate(purchasePrice, stockPrice);
+            if (isLessThan(dipRate, dropRate)) {
+                // TODO 추가 매수 및
+                return;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isLessThan(BigDecimal basePrice, BigDecimal comparePrice) {
+        return basePrice.compareTo(comparePrice) < 0;
+    }
+
+    private BigDecimal calculateDropRate(BigDecimal basePrice, BigDecimal comparePrice) {
+        if (isLessThan(basePrice, comparePrice)) {
+            throw new IllegalArgumentException("basePrice가 comparePrice보다 커야 합니다. basePrice: "
+                    + basePrice + ", comparePrice: " + comparePrice);
+        }
+
+        return basePrice.subtract(comparePrice)
+                .divide(basePrice, 4, RoundingMode.HALF_UP)
+                .multiply(BigDecimal.valueOf(100))
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateIncreaseRate(BigDecimal basePrice, BigDecimal comparePrice) {
+        if (isLessThan(comparePrice, basePrice)) {
+            throw new IllegalArgumentException("basePrice가 comparePrice보다 작아야 합니다. basePrice: "
+                    + basePrice + ", comparePrice: " + comparePrice);
+        }
+
+        return comparePrice.subtract(basePrice)
+                .divide(basePrice, 4, RoundingMode.HALF_UP)
+                .multiply(BigDecimal.valueOf(100))
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+}
